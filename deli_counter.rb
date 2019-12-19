e8a9990ea371dcf3abce23e8304db67b20bc8022
James Bridges <masterbridges@gmail.com>
2019-12-19 17:40:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,13 +5,13 @@ def line(deli)
   if deli.size == 0
     puts "The line is currently empty."
   else
-    deli = deli.each_with_index.map do |name, i|
-      "#{i+1}. #{name}"
-    end
-    puts "The line is currently: #{deli.join(" ")}"
-    #  deli.each_with_index do |name, i|
-    #    puts " #{i+1}. #{name}"
-    #  end
+    #deli = deli.each_with_index.map do |name, i|
+    #  "#{i+1}. #{name}"
+    #end
+    print "The line is currently: "
+      deli.each_with_index do |name, i|
+        print " #{i+1}. #{name}"
+      end
   end
 end
 
@@ -19,3 +19,5 @@ def take_a_number(deli, name)
   deli << name
   puts "Welcome, #{name}. You are number #{deli.size} in line."
 end
+
+

854739a2c170f6454900a6ef5bc7ffa9c893780d
James Bridges <masterbridges@gmail.com>
2019-12-19 17:00:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,10 +5,10 @@ def line(deli)
   if deli.size == 0
     puts "The line is currently empty."
   else
-    puts "The line is currently: #{  deli.each_with_index do |name, i|
-      "#{i+1}. #{name} " end}"
-
-
+    puts "The line is currently: "
+      deli.each_with_index do |name, i|
+      puts "#{i+1}. #{name} "
+    end
   end
 end
 

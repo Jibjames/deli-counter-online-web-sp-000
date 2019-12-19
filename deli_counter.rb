5582d2ddf1b80cce795b3039f766879723bd3926
James Bridges <masterbridges@gmail.com>
2019-12-19 17:43:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,3 +19,12 @@ def take_a_number(deli, name)
   deli << name
   puts "Welcome, #{name}. You are number #{deli.size} in line."
 end
+
+def now_serving(deli)
+  if deli.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "currently serving #{deli[0]}"
+  end
+  return deli.shift()
+end

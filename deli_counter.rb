27be927472aadc6c3b9636fe2bd5e1754614005c
James Bridges <masterbridges@gmail.com>
2019-12-19 16:42:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,10 @@ def line(deli)
   if deli.size == 0
     puts "The line is currently empty."
   else
-    deli.each do |person|
-      puts person
+    deli.each do |name|
+      puts name
     end
   end
 end
+
+def take_a_number(deli, name)

d954c8481c634c7fad1519d2d4fc0187a52a1377
James Bridges <masterbridges@gmail.com>
2019-12-19 16:36:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+katz_deli = []
+
+def line(deli) do
+  if deli.size == 0
+    puts "The line is currently empty."
+  else
+    deli.each |person| do
+      puts person
+    end
+  end
+end

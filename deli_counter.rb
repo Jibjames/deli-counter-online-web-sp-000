ced61b52c0eba0e20820d609dead2bc53ee8f320
James Bridges <masterbridges@gmail.com>
2019-12-19 17:18:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,18 +2,14 @@
 katz_deli = []
 
 def line(deli)
-
-  def display(deli)
-    deli.each_with_index do |name, i|
-      puts "#{i+1}. #{name} "
-    end
-  end
-
   if deli.size == 0
     puts "The line is currently empty."
   else
-    puts "The line is currently: #{display(deli)}"
-
+    do puts "The line is currently:"
+      deli.each_with_index do |name, i|
+        puts " #{i+1}. #{name}"
+      end
+    end
   end
 end
 
